a6dc53506e18db07bc8c6d6eac6f2ca9ec79220e
Suryrajsinh Jadeja <suryrajsinhjadeja95@gmail.com>
2024-07-16 10:35:36 +0530

    Add new route

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('Hello, Kubernetes!');
 });
 
+app.get('/newroute', (req, res) => {
+    res.send('This is a new route!');
+});
+
+
 app.listen(port, () => {
     console.log(`App running at http://localhost:${port}`);
 });
